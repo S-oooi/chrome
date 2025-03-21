9bc2d4d13438e8c12b74225916ecc680b98e5085
S-oooi <START01001@outlook.com>
2025-03-21 19:11:59 +0800

    解开background.js代码行的注释

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type === 'COPIED_TEXT') {
+      const copiedText = request.text;
+      console.log('Background copied text saved:', copiedText);
+    }})
